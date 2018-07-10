c1c838c3c9428c3bb3a6c25fc92c200dcf81e458
Igor Kłusak <igor.klusak@gmail.com>
2018-07-10 16:37:43 +0200

    add few statements

--- js/app.js
+++ js/app.js
@@ -10,7 +10,7 @@ addEventListener("DOMContentLoaded", function (event) {
             this.board.style.width = this.width * 10 + "px";
             this.board.style.height = this.height * 10 + "px";
             for (let j = 0; j < boardHeight; j++) {
-                this.cells[j]=[];
+                this.cells[j] = [];
                 for (let i = 0; i < this.width; i++) {
                     let singleDiv = document.createElement("div");
                     this.board.appendChild(singleDiv);
@@ -22,11 +22,54 @@ addEventListener("DOMContentLoaded", function (event) {
             }
         }
 
+        this.setCellState = (x, y, state) => {
+            this.cells[x][y].className = state;
+        }
+        this.firstGlider = () => {
+            this.setCellState(0, 1, "live")
+            this.setCellState(1, 2, "live")
+            this.setCellState(2, 0, "live")
+            this.setCellState(2, 1, "live")
+            this.setCellState(2, 2, "live")
+        };
 
+
+        this.computeCellNextState = (x, y) => {
+
+            this.neigbours = [
+                this.cells[x - 1][y - 1],
+                this.cells[x + 1][y + 1],
+                this.cells[x - 1][y + 1],
+                this.cells[x + 1][y - 1],
+                this.cells[x - 1][y],
+                this.cells[x + 1][y],
+                this.cells[x][y - 1],
+                this.cells[x][y + 1]
+            ];
+            let count = 0;
+            for (let i = 0; i < this.neigbours.length; i++) {
+                if (this.neigbours[i].className === "live") {
+                    count++
+                }
+
+            }
+            if ((this.cells[x][y].className === "" && count === 3) ||
+                (this.cells[x][y].className = "live" && count === 2 || count === 3)) {
+                return 1;
+            } else {
+                return 0;
+            }
+
+        }
     }
 
     let game = new GameOfLife(10, 10);
-    console.log(game.cells);
+
     game.createBoard();
+    console.log(game.cells);
+    console.log(game.cells[2][1]);
+    game.firstGlider()
+    console.log(game.computeCellNextState(2,1))
+
 
 });
